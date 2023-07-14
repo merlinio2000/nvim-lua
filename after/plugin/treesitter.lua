0d001f53c93fdf600614f5de77d02acbc2d0e9fd
= <merlin@merlinio.ch>
2023-07-14 14:04:07 +0200

    correct installed TreeSitter parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "lua", "c", "cpp", "rust", "python", "javascript", "typescript" },
+  ensure_installed = { "vimdoc", "lua", "c", "cpp", "rust", "python", "javascript", "typescript", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
