3330442cc79c58206bfb1be0799c66a3eee04c1a
Merlin Maggi <merlin.maggi@jls.ch>
2023-09-27 12:02:16 +0200

    update lspsaga configuration according to latest docs

--- lua/merlinio/lazyconfigs/lspsuga/saga.lua
+++ lua/merlinio/lazyconfigs/lspsuga/saga.lua
@@ -2,108 +2,74 @@ local keymap = vim.keymap.set
 local saga = require('lspsaga')
 
 saga.setup({
-    -- Options with default value
     -- "single" | "double" | "rounded" | "bold" | "plus"
-    border_style = "single",
-    --the range of 0 for fully opaque window (disabled) to 100 for fully
-    --transparent background. Values between 0-30 are typically most useful.
-    saga_winblend = 0,
-    -- when cursor in saga window you config these to move
-    move_in_saga = { prev = '<C-p>', next = '<C-n>' },
-    -- Error, Warn, Info, Hint
-    -- use emoji like
-    -- { "😡", "😥", "😤", "😐" }
-    -- and diagnostic_header can be a function type
-    -- must return a string and when diagnostic_header
-    -- is function type it will have a param `entry`
-    -- entry is a table type has these filed
-    -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
-    diagnostic_header = { " ", " ", " ", "ﴞ " },
-    -- preview lines above of lsp_finder
-    preview_lines_above = 0,
-    -- preview lines of lsp_finder and definition preview
-    max_preview_lines = 10,
-    -- use emoji lightbulb in default
-    code_action_icon = "💡",
-    -- if true can press number to execute the codeaction in codeaction window
-    code_action_num_shortcut = true,
-    -- same as nvim-lightbulb but async
-    code_action_lightbulb = {
-        enable = true,
-        enable_in_insert = true,
-        cache_code_action = true,
-        sign = false,
-        update_time = 150,
-        sign_priority = 20,
-        virtual_text = true,
+    border = "single",
+    scroll_preview = { scroll_up = '<C-p>', scroll_down = '<C-n>' },
+    hover = {
+        -- open a link in hover window with this key
+        open_link = "gx"
+        -- open_cmd = '!firefox' could be used to override
     },
-    -- finder icons
-    finder_icons = {
-        def = '  ',
-        ref = '諭 ',
-        link = '  ',
+    diagnostic = {
+        show_code_action = true,
+        jump_num_shortcut = true,
+        extend_relatedInformation = true,
+        keys = {
+            quit = { 'q', '<ESC>' },
+            quit_in_show = { 'q', '<ESC>' },
+            exec = '<CR>',
+        }
     },
-    -- finder do lsp request timeout
-    -- if your project is big enough or your server very slow
-    -- you may need to increase this value
-    finder_request_timeout = 1500,
-    finder_action_keys = {
-        open = { 'o', '<CR>' },
-        vsplit = 's',
-        split = 'h',
-        tabe = 't',
-        quit = { 'q', '<ESC>' },
+    finder = {
+        finder_action_keys = {
+            toggle_or_open = { 'o', '<CR>' },
+            vsplit = 's',
+            split = 'h',
+            tabe = 't',
+            close = { 'q', '<ESC>' },
+            quit = { 'q', '<ESC>' },
+        },
     },
-    code_action_keys = {
-        quit = 'q',
-        exec = '<CR>',
+    code_action = {
+        num_shortcut = true,
+        show_server_name = true,
+        keys = {
+            quit = { 'q', '<ESC>' },
+            exec = '<CR>',
+        }
     },
-    definition_action_keys = {
-        edit = '<C-c>o',
-        vsplit = '<C-c>v',
-        split = '<C-c>i',
-        tabe = '<C-c>t',
-        quit = { 'q', '<ESC>' },
+    definition = {
+        keys = {
+            edit = '<C-c>o',
+            vsplit = '<C-c>v',
+            split = '<C-c>i',
+            tabe = '<C-c>t',
+            quit = { 'q', '<ESC>' },
+        }
     },
-    rename_action_quit = '<ESC>',
-    rename_in_select = true,
-    -- show symbols in winbar must nightly
-    -- in_custom mean use lspsaga api to get symbols
-    -- and set it to your custom winbar or some winbar plugins.
-    -- if in_cusomt = true you must set in_enable to false
+    rename = {
+        in_select = true,
+        keys = {
+            quit = { 'q', '<ESC>' },
+        }
+    },
     symbol_in_winbar = {
-        in_custom = false,
         enable = true,
         separator = ' ',
         show_file = true,
-        -- define how to customize filename, eg: %:., %
-        -- if not set, use default value `%:t`
-        -- more information see `vim.fn.expand` or `expand`
-        -- ## only valid after set `show_file = true`
-        file_formatter = "",
-        click_support = false,
+        folder_level = 4,
     },
-    -- show outline
-    show_outline = {
+    outline = {
         win_position = 'right',
-        --set special filetype win that outline window split.like NvimTree neotree
-        -- defx, db_ui
-        win_with = '',
         win_width = 30,
         auto_enter = true,
         auto_preview = true,
-        virt_text = '┃',
-        jump_key = 'o',
-        -- auto refresh when change buffer
         auto_refresh = true,
-    },
-    -- custom lsp kind
-    -- usage { Field = 'color code'} or {Field = {your icon, your color code}}
-    custom_kind = {},
-    -- if you don't use nvim-lspconfig you must pass your server name and
-    -- the related filetypes into this table
-    -- like server_filetype_map = { metals = { "sbt", "scala" } }
-    server_filetype_map = {},
+        keys = {
+            toggle_or_jump = 'o',
+            quit = { 'q', '<ESC>' },
+        }
+    }
 })
 
 -- Lsp finder find the symbol definition implement reference
@@ -147,3 +113,5 @@ keymap("n", "so", "<cmd>Lspsaga outline<CR>", { silent = true })
 
 -- Hover Doc
 keymap("n", "sk", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+-- keeps hover window open pinned to the right
+keymap("n", "sK", "<cmd>Lspsaga hover_doc ++keep<CR>", { silent = true })
