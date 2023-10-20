d971925441fd5d5b450f48489ea2e60ac608f9a1
= <merlin@merlinio.ch>
2023-10-21 01:35:07 +0200

    add rustfmt

--- lua/merlinio/lazyconfigs/format.lua
+++ lua/merlinio/lazyconfigs/format.lua
@@ -24,6 +24,7 @@ return {
 			format_on_save = { timeout_ms = 500, lsp_fallback = true },
 			---@type table<string, conform.FormatterUnit[]>
 			formatters_by_ft = {
+				rust = { "rustfmt" },
 				lua = { "stylua" },
 				sh = { "shfmt" },
 				javascript = { "prettierd" },
