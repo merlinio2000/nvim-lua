80b2715b38e3a44dc450f8314dac0b40bea44958
= <merlin@merlinio.ch>
2023-11-27 18:57:10 +0100

    enable flash jump

--- lua/merlinio/lazyconfigs/flash/keys.lua
+++ lua/merlinio/lazyconfigs/flash/keys.lua
@@ -1,8 +1,42 @@
 return {
-    -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" }, -- TODO think of key clash with lspsaga
-    { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-    { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" }, -- TODO how to use this effectively
-    { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc =
-    "Toggle Flash Search" },
+	{
+		"s",
+		mode = { "n", "x", "o" },
+		function()
+			require("flash").jump()
+		end,
+		desc = "Flash",
+	},
+	{
+		"S",
+		mode = { "n", "o", "x" },
+		function()
+			require("flash").treesitter()
+		end,
+		desc = "Flash Treesitter",
+	},
+	{
+		"r",
+		mode = "o",
+		function()
+			require("flash").remote()
+		end,
+		desc = "Remote Flash",
+	}, -- TODO how to use this effectively
+	{
+		"R",
+		mode = { "o", "x" },
+		function()
+			require("flash").treesitter_search()
+		end,
+		desc = "Treesitter Search",
+	},
+	{
+		"<c-s>",
+		mode = { "c" },
+		function()
+			require("flash").toggle()
+		end,
+		desc = "Toggle Flash Search",
+	},
 }
