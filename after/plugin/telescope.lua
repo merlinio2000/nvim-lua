1d1c47ee4d67924d42b8f081babe1a06e3bcb05b
= <merlin@merlinio.ch>
2023-11-27 18:09:18 +0100

    enable live_grep_args for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,24 @@
 local tel = require("telescope")
+local lga_actions = require("telescope-live-grep-args.actions")
+
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
 tel.setup({
 	extensions = {
+		live_grep_args = {
+			auto_quoting = true, -- enable/disable auto-quoting
+			-- define mappings, e.g.
+			mappings = { -- extend mappings
+				i = {
+					["<C-q>"] = lga_actions.quote_prompt(),
+					["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+				},
+			},
+			-- ... also accepts theme settings, for example:
+			-- theme = "dropdown", -- use dropdown theme
+			-- theme = { }, -- use own theme spec
+			-- layout_config = { mirror=true }, -- mirror preview pane
+		},
 		fzf = {
 			fuzzy = true, -- false will only do exact matching
 			override_generic_sorter = true, -- override the generic sorter
@@ -12,15 +28,32 @@ tel.setup({
 		},
 	},
 })
+
+tel.load_extension("live_grep_args")
+
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 tel.load_extension("fzf")
-
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-vim.keymap.set("n", "<leader>ft", builtin.live_grep, { desc = "Find Text" })
-vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find Old Files (previously used)" })
+vim.keymap.set(
+	"n",
+	"<leader>ft",
+	":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+	{ desc = "Find Text" }
+)
+local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+vim.keymap.set("n", "<leader>fc", live_grep_args_shortcuts.grep_word_under_cursor, { desc = "Find word under Cursor" })
+vim.keymap.set(
+	"v",
+	"<leader>fv",
+	live_grep_args_shortcuts.grep_visual_selection,
+	{ desc = "Find current Visual selection" }
+)
+
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
+
+vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find Old Files (previously used)" })
 
 vim.keymap.set("n", "<leader>fgf", builtin.git_files, { desc = "Find Git Files" })
 vim.keymap.set("n", "<leader>fgb", builtin.git_branches, { desc = "Find Git Branches" })
--- lua/merlinio/lazy.lua
+++ lua/merlinio/lazy.lua
@@ -204,7 +204,15 @@ require("lazy").setup({
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				-- This will not install any breaking changes.
+				-- For major updates, this must be adjusted manually.
+				version = "^1.0.0",
+			},
+		},
 		lazy = false,
 	},
 	{
