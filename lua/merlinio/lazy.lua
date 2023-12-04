fe2f80639a25441d5fdb3fc59a5768cef0164d2a
= <merlin@merlinio.ch>
2023-12-04 20:18:22 +0100

    update dependencies and fix LuaSnip semver

--- lua/merlinio/lazy.lua
+++ lua/merlinio/lazy.lua
@@ -19,7 +19,12 @@ require("lazy").setup({
 	},
 	{ "mbbill/undotree", lazy = false },
 	-- Snippets
-	{ "L3MON4D3/LuaSnip" },
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+	},
 	{ "saadparwaiz1/cmp_luasnip" },
 	{ "rafamadriz/friendly-snippets" },
 	-- LSP helper
