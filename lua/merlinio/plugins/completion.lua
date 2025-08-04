724c505d29edd5c27e1fdce3fa934c616d40458e
Merlin Maggi <merlin.maggi@jls.ch>
2025-08-04 16:33:00 +0200

    ui: add db client

--- lua/merlinio/plugins/completion.lua
+++ lua/merlinio/plugins/completion.lua
@@ -49,12 +49,21 @@ return {
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+			per_filetype = {
+				sql = { "dadbod", "path", "snippets", "buffer" },
+			},
+
 			providers = {
 				lazydev = {
 					name = "LazyDev",
 					module = "lazydev.integrations.blink",
 					-- make lazydev completions top priority (see `:h blink.cmp`)
 					score_offset = 100,
+				},
+
+				dadbod = {
+					name = "Dadbod",
+					module = "vim_dadbod_completion.blink",
 				},
 			},
 		},
